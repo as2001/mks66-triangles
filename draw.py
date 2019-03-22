bfec646b006320baec798d8013fbed2a098f9a69
Andrew Shao <ashao1@stuy.edu>
2019-03-22 15:36:51 -0400

    idk

--- draw.py
+++ draw.py
@@ -25,24 +25,29 @@ def draw_polygons( polygons, screen, color ):
 
 
 def add_box( polygons, x, y, z, width, height, depth ):
-    x1 = x + width
+     x1 = x + width
     y1 = y - height
     z1 = z - depth
 
     #front
-    add_polygon(
-    add_edge(polygons, x, y, z, x1, y, z)
-    add_edge(polygons, x, y1, z, x1, y1, z)
-    add_edge(polygons, x1, y, z, x1, y1, z)
-    add_edge(polygons, x, y, z, x, y1, z)
+    add_polygon(polygons,x,y,z,x1,y,z,x,y1,z)
+    add_polygon(polygons,x1,y,z,x,y1,z,x1,y1,z)
+##    add_edge(polygons, x, y, z, x1, y, z)
+##    add_edge(polygons, x, y1, z, x1, y1, z)
+##    add_edge(polygons, x1, y, z, x1, y1, z)
+##    add_edge(polygons, x, y, z, x, y1, z)
 
     #back
-    add_edge(polygons, x, y, z1, x1, y, z1)
-    add_edge(polygons, x, y1, z1, x1, y1, z1)
-    add_edge(polygons, x1, y, z1, x1, y1, z1)
-    add_edge(polygons, x, y, z1, x, y1, z1)
+    add_polygon(polygons,x,y,z1,x1,y,z1,x,y1,z1)
+    add_polygon(polygons,x1,y,z1,x,y1,z1,x1,y1,z1)
+##    add_edge(polygons, x, y, z1, x1, y, z1)
+##    add_edge(polygons, x, y1, z1, x1, y1, z1)
+##    add_edge(polygons, x1, y, z1, x1, y1, z1)
+##    add_edge(polygons, x, y, z1, x, y1, z1)
 
     #sides
+    add_polygon(polygons,x,y,z,x,y,z1,x1,y,z)
+    add_polygon(polygons,x,y,z1,x1,y,z,
     add_edge(polygons, x, y, z, x, y, z1)
     add_edge(polygons, x1, y, z, x1, y, z1)
     add_edge(polygons, x, y1, z, x, y1, z1)
